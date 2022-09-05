eacaf2a86142a699faad585eabb1f00051fc4a4b
A S M Rizwan Chowdhury <112667292+RizwanLikhon@users.noreply.github.com>
2022-09-04 20:14:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ArrowShop;
+
+//Creating List
+List <ArrowParts>alistofarrows=new List <ArrowParts> ();
+
+
+//Here comes the Constructor
+ArrowParts arrowparts1 = new ArrowParts("n/a",0,0,0);
+
+
+//adding into List
+alistofarrows.Add (arrowparts1);
+
+
+
+//Looping through the list and printing them out
+foreach (var a in alistofarrows)
+{
+    Console.WriteLine(a.ToString());
+    Console.WriteLine("\n");
+}
+
